b9967137f0ee0f73e74db88cc8ac08417e6327ca
Dani <danichilling@gmail.com>
2024-06-21 18:10:13 +0100

    useState for selected letters created

--- src/HangmanGame.tsx
+++ src/HangmanGame.tsx
@@ -1,11 +1,27 @@
-// import { useState } from "react";
+//@ts-ignore
+import { useState } from "react";
 
 function HangmanGame() {
   const wordToGuess = ["m", "e", "l", "_", "n"];
-  //   const [selectedLetters, setSelectedLetters] = useState([]);
+  const [selectedLetters, setSelectedLetters] = useState<string[]>([
+    "c",
+    "a",
+    "t",
+    "s",
+  ]);
+
+  function storeSelectedLetter(inputLetter: string) {
+    const copyArray = [...selectedLetters, inputLetter];
+    setSelectedLetters(copyArray);
+    console.log(selectedLetters);
+  }
 
   const keyboard = alphabet.map((letter) => (
-    <span className="keyboard-letter" onClick={() => console.log(letter)}>
+    <span
+      key={letter}
+      className="keyboard-letter"
+      onClick={() => storeSelectedLetter(letter)}
+    >
       {letter}
     </span>
   ));
